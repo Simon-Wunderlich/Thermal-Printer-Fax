198c60c7ad717a0bdeffd5fbefe863ab3d335df3
Simon Wunderlich <simongwunderlich@gmail.com>
2026-05-05 08:52:20 +1000

    Create seperate threads for websocket recieving and broadcasting

--- server.py
+++ server.py
@@ -1,29 +1,37 @@
-import asyncio
-
 import websockets
 from websockets import broadcast
-
+from websockets.sync.server import serve
 from renderer import Renderer
+import threading
 
 CLIENTS = set()
 
 async def handler(websocket):
     CLIENTS.add(websocket)
+    try:
+        async for message in websocket:
+            await websocket.send(f"Echo: {message}")
+    finally:
+        CLIENTS.remove(websocket)
+
+def main():
+    with serve(handler, host="0.0.0.0", port=8765) as server:
+       server.serve_forever()  # run forever
+
+def broadcastMessage():
     renderer = Renderer()
     while True:
         text = input("> ")
         textImage = renderer.create_text(text)
         buf = renderer.processImage(textImage)
         broadcast(CLIENTS, buf)
-    # try:
-    #     async for message in websocket:
-    #         await websocket.send(f"Echo: {message}")
-    # finally:
-    #     CLIENTS.remove(websocket)
 
-async def main():
-    async with websockets.serve(handler, host="0.0.0.0", port=8765):
-        await asyncio.Future()  # run forever
 
 if __name__ == "__main__":
-    asyncio.run(main())
+   ws = threading.Thread(target=main)
+   bc = threading.Thread(target=broadcastMessage)
+   ws.start()
+   bc.start()
+   ws.join()
+   bc.join()
+
