bd20e69a5c9e53dedcaa3d58c05d38210c0501d4
Akira-Ellis <88813407+Akira-Ellis@users.noreply.github.com>
2026-06-27 13:22:38 +1000

    renderer.py
    added header rendering

--- renderer.py
+++ renderer.py
@@ -1,23 +1,29 @@
 import base64
+import datetime
 import struct
-import PIL
-from PIL import Image, ImageOps, ImageChops, ImageDraw, ImageFont
 from io import BytesIO
+
+import PIL
+from PIL import Image, ImageChops, ImageDraw, ImageFont, ImageOps
+
 
 class Renderer:
     printerWidth = 384
 
     def trimImage(self, im):
-        bg = PIL.Image.new(im.mode, im.size, (255,255,255))
+        bg = PIL.Image.new(im.mode, im.size, (255, 255, 255))
         diff = ImageChops.difference(im, bg)
         diff = ImageChops.add(diff, diff, 2.0)
         bbox = diff.getbbox()
         if bbox:
-            return im.crop((bbox[0],bbox[1],bbox[2],bbox[3]+10)) # don't cut off the end of the image
+            return im.crop(
+                (bbox[0], bbox[1], bbox[2], bbox[3] + 10)
+            )  # don't cut off the end of the image
 
     # TODO: UPDATE TO INCLUDE IMAGE
     def create_text(self, text, font_name="Lucon.ttf", font_size=32):
-        img = PIL.Image.new('RGB', (self.printerWidth, 5000), color = (255, 255, 255))
+        time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        img = PIL.Image.new("RGB", (self.printerWidth, 5000), color=(255, 255, 255))
         font = ImageFont.truetype(font_name, font_size)
 
         d = ImageDraw.Draw(img)
@@ -25,7 +31,7 @@ class Renderer:
         for line in text.splitlines():
             lines.append(self.get_wrapped_text(line, font, self.printerWidth))
         lines = "\n".join(lines)
-        d.text((0,0), lines, fill=(0,0,0), font=font)
+        d.text((0, 0), lines, fill=(0, 0, 0), font=font)
         return self.trimImage(img)
 
     def create_attachment(self, data):
@@ -39,39 +45,45 @@ class Renderer:
     def createBody(self, message):
         text = None
         image = None
-        if (message["msg"] != ""):
+        header = None
+        if message["msg"] != "":
             text_raw = self.create_text(message["msg"])
             text = self.processImage(text_raw)
 
-        if (message["img"] != ""):
+        if message["img"] != "":
             image_raw = self.create_attachment(message["img"])
             image = self.processImage(image_raw)
 
+        if message["header"] != "":
+            header_raw = self.create_text(message["header"])
+            header = self.processImage(header_raw)
+
         textHeight = text.height if text is not None else 0
         imgHeight = image.height if image is not None else 0
-        totalHeight = textHeight + imgHeight
+        headerHeight = header.height if header is not None else 0
+        totalHeight = headerHeight + textHeight + imgHeight
 
-        new_im = Image.new('1', (self.printerWidth, totalHeight))
+        new_im = Image.new("1", (self.printerWidth, totalHeight))
 
+        if header is not None:
+            new_im.paste(header, (0, 0))
         if image is not None:
-            new_im.paste(image, (0,0))
+            new_im.paste(image, (0, 0))
         if text is not None:
             new_im.paste(text, (0, imgHeight))
         return new_im
 
-
-
-    def get_wrapped_text(self, text: str, font: PIL.ImageFont.ImageFont,
-                         line_length: int):
-        lines = ['']
+    def get_wrapped_text(
+        self, text: str, font: PIL.ImageFont.ImageFont, line_length: int
+    ):
+        lines = [""]
         for word in text.split():
-            line = f'{lines[-1]} {word}'.strip()
+            line = f"{lines[-1]} {word}".strip()
             if font.getlength(line) <= line_length:
                 lines[-1] = line
             else:
                 lines.append(word)
-        return '\n'.join(lines)
-
+        return "\n".join(lines)
 
     def processImage(self, im):
         if im.width > self.printerWidth:
@@ -85,34 +97,32 @@ class Renderer:
             padded_image.paste(im)
             im = padded_image
 
-        im = im.rotate(180) #print it so it looks right when spewing out of the mouth
+        im = im.rotate(180)  # print it so it looks right when spewing out of the mouth
 
         # if image is not 1-bit, convert it
-        if im.mode != '1':
-            im = im.convert('1')
-
+        if im.mode != "1":
+            im = im.convert("1")
 
         # if image width is not a multiple of 8 pixels, fix that
         if im.size[0] % 8:
-            im2 = Image.new('1', (im.size[0] + 8 - im.size[0] % 8,
-                                  im.size[1]), 'white')
+            im2 = Image.new("1", (im.size[0] + 8 - im.size[0] % 8, im.size[1]), "white")
             im2.paste(im, (0, 0))
             im = im2
 
-
-
         # Invert image, via greyscale for compatibility
         #  (no, I don't know why I need to do this)
-        im = ImageOps.invert(im.convert('L'))
+        im = ImageOps.invert(im.convert("L"))
         # ... and now convert back to single bit
-        im = im.convert('1')
+        im = im.convert("1")
         return im
 
     def getBuffer(self, im):
-        buf = b''.join((bytearray(b'\x1d\x76\x30\x00'),
-                        struct.pack('2B', int(im.size[0] / 8 % 256),
-                                    int(im.size[0] / 8 / 256)),
-                        struct.pack('2B', int(im.size[1] % 256),
-                                    int(im.size[1] / 256)),
-                        im.tobytes()))
+        buf = b"".join(
+            (
+                bytearray(b"\x1d\x76\x30\x00"),
+                struct.pack("2B", int(im.size[0] / 8 % 256), int(im.size[0] / 8 / 256)),
+                struct.pack("2B", int(im.size[1] % 256), int(im.size[1] / 256)),
+                im.tobytes(),
+            )
+        )
         return buf
